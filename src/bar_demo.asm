6c03249d6826b5a0ea8302765809f7b72d180c5b
Thomas40522 <73463155+Thomas40522@users.noreply.github.com>
2025-04-09 13:35:40 -0400

    updated assembly code

--- src/bar_demo.asm
+++ src/bar_demo.asm
@@ -1,20 +1,26 @@
-; bar_demo.asm (version 4)
-; Updated 01APR2025
+; bar_demo.asm (version 6)
+; Updated 09APR2025
 ; Ouputs a continuously changing value whose increment is determined by switches
 ; The leftmost switch makes the increment negative
+; The User set the max bar number when the program initialize, they have to then lower all the switches to start the program
 
 ORG 0
+Set:
+	IN		Switches
+    JZERO	Set
+    OUT		Bar_max
+
+Wait:
+	In		Switches
+    JZERO	Loop
+    JUMP	Wait
+
 Loop:
-	IN	Switches
+	IN		Switches
 	STORE	Increment
 	SHIFT	-9
-	JZERO	Positive
-	LOAD	Increment
-	SUB	Bit9
-	STORE	Increment
-	LOAD	Total
-	SUB	Increment
-	JUMP	Display
+	JZERO	Positive    
+	JUMP	Negative
 
 Positive:
 	LOAD	Increment
@@ -22,16 +28,28 @@ Positive:
 	LOAD	Total
 	ADDI	1
     STORE	Total
-	OUT	Bar
+	OUT		Bar
 	LOAD	Increment
 	ADDI	-1
 	STORE	Increment
 	JPOS	Positive
     
+Negative:
+	LOAD	Increment
+	JZERO	Display
+	LOAD	Total
+	ADDI	-1
+    STORE	Total
+	OUT		Bar
+	LOAD	Increment
+	ADDI	1
+	STORE	Increment
+	JNEG	Negative
+    
 Display:
 	LOAD	Total
-    OUT	Bar
-	OUT	Hex0
+    OUT		Bar
+	OUT		Hex0
 	STORE   Total
 	CALL    Delay
 	JUMP 	Loop
@@ -61,3 +79,4 @@ Timer:     EQU 002
 Hex0:      EQU 004
 Hex1:      EQU 005
 Bar:	   EQU 006
+Bar_max:   EQU 007
